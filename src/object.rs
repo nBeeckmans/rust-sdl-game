53c1ac67ca70d904c79c9f58fcdfa5e7bfc1343f
Nicolas Beeckmans <beeckmans.nicolas@ens.uqam.ca>
2025-04-12 22:21:50 -0400

    Complete object struct and impl

--- src/object.rs
+++ src/object.rs
@@ -1,25 +1,64 @@
+use std::fmt::Pointer;
+use std::ops::Deref;
+use std::rc::Rc;
+use std::cell::RefCell;
 use sdl3::render::WindowCanvas;
 use crate::drawable::Drawable;
 use crate::transformable::Transformable;
 
 pub struct Object {
-    pub children : Vec<Box<dyn Drawable>>,
-    pub position : (i32, i32),
+    primitives : Vec<Box<dyn Drawable>>,
+    children : RefCell<Vec<Rc<Object>>>,
+    position : (i32, i32),
+    z: i32,
 }
 
 impl Drawable for Object {
     fn draw(&self, window_canvas: &mut WindowCanvas) {
-        for i in &self.children {
+        for i in &self.primitives {
             i.draw(window_canvas);
         }
     }
 }
 
 impl Transformable for Object {
-    fn rotate(&self, by: f32) {
-
+    fn rotate(&mut self, by: f32) {
+        let mut r = (self.position.0 * self.position.0 + self.position.1 * self.position.1) as f32;
+        r = r.sqrt();
+        self.position.0 += (r * by.cos()).round() as i32;
+        self.position.1 += (r * by.sin()).round() as i32;
     }
 
-    fn translate(&self, by: (i32, i32)) {
+    fn translate(&mut self, by: (i32, i32)) {
+        self.position.0 += by.0;
+        self.position.1 += by.1;
+    }
+}
+
+impl Object {
+    pub fn new(position : (i32, i32), z : i32 ) -> Rc<Object> {
+        let mut object = Rc::new(Object {
+            primitives : Vec::new(),
+            children : RefCell::new(Vec::new()),
+            position,
+            z,
+        });
+        object.children.borrow_mut().push(object.clone());
+        return object;
+    }
+    pub fn add_primitive(&mut self, primitive : Box<dyn Drawable>) {
+        self.primitives.push(primitive);
+    }
+    pub fn add_child(&mut self, child: Rc<Object>) {
+        let mut index = -1;
+        for current_child in child.children.borrow_mut().iter() {
+            index += 1;
+            if (current_child.z > child.z ) {
+
+            }
+        }
+        if (index == -1) {
+            self.children.get_mut().push(child);
+        }
     }
 }
